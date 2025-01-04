7b61248f63208402209d8ad18b77d174f869b6c9
Zapd0r <69820567+Zapd0r@users.noreply.github.com>
2025-01-04 21:41:43 +0100

    Working Etch-A-Sketch

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 const container = document.getElementById("container")
+let gridSize = 32
+var pointerDown = false
+var currentColor = "#1a1a1a"
+
+document.addEventListener("mousedown", () => {
+  pointerDown = true
+})
+document.addEventListener("mouseup", () => {
+  pointerDown = false
+})
 
 function createGrid(gridSize) {
-  for (let i = 0; i < gridSize*2; i++) { 
+  for (let i = 0; i < gridSize*gridSize; i++) { 
     let squares = document.createElement("div")
     squares.classList.add("squares")
+    squares.addEventListener("mouseover", (element) => {
+      if (pointerDown) {
+        element.target.style.backgroundColor = currentColor
+      } 
+    })
+    // squares.addEventListener("mouseleave", (element) => {
+    //   setTimeout(() => {
+    //     element.target.style.backgroundColor = "transparent"}, 500)
+    // })
     container.appendChild(squares)
   }
+
+  container.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
+  container.style.gridTemplateRows = `repeat(${gridSize},1fr)`
+}
+
+createGrid(gridSize)
+
+function resizeGrid() {
+  gridSize = parseInt(prompt("Please input the grid size, Input between 10 and 100")) 
+
+  if (gridSize < 10 || gridSize > 100) {
+    resizeGrid()
+    return
+  }
+  
+  container.replaceChildren()
+
+  createGrid(gridSize)
+}
+
+function reset() {  
+  container.childNodes.forEach(element => {
+    element.style.backgroundColor = "transparent"
+  });
 }
