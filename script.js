041c3c88a41f36e038515b1c425eb826b89cf824
Zapd0r <69820567+Zapd0r@users.noreply.github.com>
2025-01-05 09:31:58 +0100

    added turning knobs when using the mouse

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const container = document.getElementById("grid-container")
 const colorpicker = [... document.querySelectorAll(".colorpicker")]
-
+const knobX = document.getElementById("knobX")
+const knobY = document.getElementById("knobY")
 
 let gridSize = 32
 var pointerDown = false
 var currentColor = "#1a1a1a"
+var mouseX, mouseY
 
-document.addEventListener("mousedown", () => {
+// enable/disable drawing when mousebutton is pressed
+container.addEventListener("mousedown", () => {
   pointerDown = true
 })
-document.addEventListener("mouseup", () => {
+container.addEventListener("mouseup", () => {
   pointerDown = false
 })
 
+document.addEventListener("mousemove", (event) => {
+  mouseX = (event.pageX ) * Math.PI
+  mouseY = (event.pageY ) * Math.PI
+
+  if(pointerDown) {
+  knobX.style.rotate = `${mouseX}deg`
+  knobY.style.rotate = `${mouseY}deg`
+}
+})
+
+// add eventlistener to colorselection and setting variable
 colorpicker.forEach(picker => {
   picker.style.backgroundColor = picker.dataset.color
   picker.addEventListener("click", ()=> {
     currentColor = picker.dataset.color
   })
-  
-});
+})
 
+// create the grid by filling it with squares that all have an eventlistener for settint the color
+// preventDefault for disable dragging of squares
 function createGrid(gridSize) {
   for (let i = 0; i < gridSize*gridSize; i++) { 
     let squares = document.createElement("div")
@@ -34,19 +49,17 @@ function createGrid(gridSize) {
     squares.addEventListener("dragstart", (event) => {
       event.preventDefault()
     })
-    // squares.addEventListener("mouseleave", (element) => {
-    //   setTimeout(() => {
-    //     element.target.style.backgroundColor = "transparent"}, 500)
-    // })
     container.appendChild(squares)
   }
 
+  // resizing the grid template based on the provided gridsize
   container.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
   container.style.gridTemplateRows = `repeat(${gridSize},1fr)`
 }
 
 createGrid(gridSize)
 
+// resize function for the button
 function resizeGrid() {
   gridSize = parseInt(prompt("Please input the grid size, Input between 10 and 100")) 
 
@@ -54,16 +67,17 @@ function resizeGrid() {
     resizeGrid()
     return
   }
-  
-  container.replaceChildren()
 
+  //clearing the current children
+  container.replaceChildren()
   createGrid(gridSize)
 }
 
+// resetfunction to clear the etch-a-sketch
 function reset() {  
   container.childNodes.forEach(element => {
     element.style.backgroundColor = "transparent"
-  });
+  })
 }
 
 function setColor(color) {
